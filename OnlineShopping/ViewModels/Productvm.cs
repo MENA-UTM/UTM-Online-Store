c1f32ca2869146c53ccab9a0cb626cd6e8e49613
AymenKhemiri06 <aymenkhemiri@graduate.utm.my>
2022-12-27 21:13:00 +0800

    ubdite

---
+++ OnlineShopping/ViewModels/Productvm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShopping.ViewModels
+{
+    public class Productvm
+    {
+        public int Prod_Id { get; set; }
+        public string Prod_Name { get; set; }
+        public string Prod_Disc { get; set; }
+        public string Purchase_Price { get; set; }
+        public string Sale_Price { get; set; }
+        public string Prod_Pic { get; set; }
+       
+        public HttpPostedFileBase Pro_Pic { get; set; }
+        
+        public int PRO_QUANTITY { get; set; }
+        public int Cat_Fid { get; set; }
+    }
+}
