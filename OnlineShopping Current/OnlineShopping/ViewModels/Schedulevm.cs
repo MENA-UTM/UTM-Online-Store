2942017316a37d863a6c03059143518c6863fca1
MohammedA-2000 <115534764+MohammedA-2000@users.noreply.github.com>
2022-12-27 22:27:41 +0800

    New update

---
+++ OnlineShopping Current/OnlineShopping/ViewModels/Schedulevm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShopping.ViewModels
+{
+    public class Schedulevm
+    {
+        
+        public int Schedule_id { get; set; }
+        public int Schedule_day { get; set; }
+        public int doc_fid { get; set; }
+
+        public int no_of_slots { get; set; }
+        public string start_time { get; set; }
+        public Nullable<int> fee { get; set; }
+    }
+}
